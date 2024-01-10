ad7cd7e68828046fb7b87233ea7d93323d58311c
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-01-10 11:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+            // DOM is loaded, add your code here
+            const body = document.body;
+            body.textContent = "DOM load success";
+        });
